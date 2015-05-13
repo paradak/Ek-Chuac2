19b09164d62e0bff5837f83ce718f32db52cfe38
paradak <raul_mister98@hotmail.com>
2015-05-13 18:54:15 -0500

    declaracion funciones base de datos

--- app/src/main/java/com/example/raul__000/myfirstapp/HistoryBD.java
+++ app/src/main/java/com/example/raul__000/myfirstapp/HistoryBD.java
@@ -4,4 +4,21 @@ package com.example.raul__000.myfirstapp;
  * Created by raul__000 on 13/05/2015.
  */
 public class HistoryBD {
+
+
+    public void consultar (){
+
+    }
+
+    public void insertar(){
+
+    }
+
+    public void eliminar (){
+
+    }
+
+    public void actulizar(){
+
+    }
 }
